c15bf3d33d096b4895ae9b45481445660a4b5f19
Peter Rojs <peter.rojs@gmail.com>
2024-05-15 23:32:10 +0200

    refactor: handle change detected in a function

--- src/processor/processor.rs
+++ src/processor/processor.rs
@@ -19,26 +19,30 @@ impl Processor {
         while let Some(command) = self.receiver.recv().await {
             match command {
                 Command::ChangeDetected { paths } => {
-                    info!("Received event");
-
-                    let database_paths = match self.database.lock().await.get_all_project_paths().await {
-                        Ok(paths) => paths,
-                        Err(err) => {
-                            info!("Failed to get database paths: {:?}", err);
-                            continue;
-                        }
-                    };
-                    info!("Database paths: {:?}", database_paths);
-
-                    let changed_project_path = Self::find_matching_path(&database_paths, &paths[0]);
-                    match changed_project_path {
-                        Some(path) => info!("Event path matches database path: {}", path),
-                        None => info!("Event path does not match any database path"),
-                    }
+                    self.handle_change_detected(paths)
                 }
                 // Add more command handling cases as needed
                 _ => {}
             }
+        }
+    }
+
+    fn handle_change_detected(&self, paths: Vec<String>) {
+        info!("Received event");
+
+        let database_paths = match self.database.lock().await.get_all_project_paths().await {
+            Ok(paths) => paths,
+            Err(err) => {
+                info!("Failed to get database paths: {:?}", err);
+                return;
+            }
+        };
+        info!("Database paths: {:?}", database_paths);
+
+        let changed_project_path = Self::find_matching_path(&database_paths, &paths[0]);
+        match changed_project_path {
+            Some(path) => info!("Event path matches database path: {}", path),
+            None => info!("Event path does not match any database path"),
         }
     }
 
