2a5fbe93d61eacac92df88aeb85608a0df4afce1
Peter Rojs <peter.rojs@gmail.com>
2024-06-16 16:17:04 +0200

    fix: another generated path change

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let mut config = prost_build::Config::new();
-    config.out_dir("src/generated");
+    config.out_dir("./src/generated");
     config.protoc_arg("--experimental_allow_proto3_optional");
     config
         .compile_protos(
